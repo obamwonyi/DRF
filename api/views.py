767ef4de702b5290f985f3d44b2c5dc65708746a
obamwonyi <developer.oduwa@gmail.com>
2024-01-29 13:45:54 +0100

    added the functionality for other routes

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 # helps manage the view with respect to url methods
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 # helps carry out http response with respect to the request
 from rest_framework.response import Response
 # this would allow us to write json objects into database
@@ -10,14 +10,30 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 # make the user model assessable for use . 
 from django.contrib.auth.models import User
+# make 
+from django.shortcuts import get_object_or_404
+# manage authentication checks
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+# check if the user making a request is authenticated
+from rest_framework.permissions import IsAuthenticated
+
+
+
 
 @api_view(['POST'])
 def login(request):
     """
     Handle The login route/url
     """
-
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({
+            "token": token.key,
+            "user": serializer.data
+        })
 
 
 @api_view(['POST'])
@@ -45,8 +61,12 @@ def signup(request):
 
 
 @api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
     """
     Handle the test_token route/url
     """
-    return Response({})
+    return Response({
+        "passed for {}".format(request.user.email)
+    })
