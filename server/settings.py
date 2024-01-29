935970a895e7a10cd4ae240749c74f9d6e6dc160
obamwonyi <developer.oduwa@gmail.com>
2024-01-29 11:07:20 +0100

    added the app api

--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'api.apps.ApiConfig',
+
+    # Rest framework packages
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
